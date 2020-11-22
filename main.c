3883b1e580a48a666244bdf24467783ed6393f28
Gaint554 <74464136+Gaint554@users.noreply.github.com>
2020-11-23 02:05:41 +0530

    Dijkstra-s-Algorithm

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // for exit function
 #define INFINITY 99999 
 
 int main()
 {
-	int cost[10][10], visited[10], dist[10], min, source_ver, totver, start_again;
+	int cost[10][10], visited[10], dist[10], min, source_node, tot_node, start_again;
 	
-	printf("Enter number of vertices: ");
-	scanf("%d", &totver);	// taking the total number of vertex from user
+	printf("Enter number of nodes: ");
+	scanf("%d", &tot_node);	// taking the total number of node from user
 
 	// Every loop in this program start from 1 so that it is easy to understand and better for the user to do input and output
 	printf("Enter cost matrix: \n");
-	for (int i = 1; i <= totver; i++)
+	for (int i = 1; i <= tot_node; i++)
 	{
-		for (int j = 1; j <= totver; j++)
+		for (int j = 1; j <= tot_node; j++)
 		{
-			scanf("%d", &cost[i][j]); // taking the distance between each vertex
+			scanf("%d", &cost[i][j]); // taking the distance between each node
 			if (cost[i][j] == 0) // checking if the user enter 0 as distance 
 			{
 				cost[i][j] = INFINITY; // making the 0 distance as INFINITY
@@ -25,32 +25,32 @@ int main()
 	
 	printf("Enter the source: ");
 	source_again: 
-	scanf("%d", &source_ver); // asking the user that which vertex he what to take as source vertex
+	scanf("%d", &source_node); // asking the user that which node he what to take as source node
 
-	if (source_ver > totver) // it pervent the user from entering invalid input 
+	if (source_node > tot_node) // it pervent the user from entering invalid input 
 	{
 		printf("\t\t*****Invaild Input*****\n");
 		printf("Please enter the source again: ");
-		goto source_again; // sending back up for taking the source vertex
+		goto source_again; // sending back up for taking the source node
 	}
 
-	for (int i = 1; i <= totver; i++)
+	for (int i = 1; i <= tot_node; i++)
 	{
 		visited[i] = 0; // it initailize the visited array with 0
-		dist[i] = cost[source_ver][i]; // it initailize the distance array with repespect to cost matrix and source vertex
+		dist[i] = cost[source_node][i]; // it initailize the distance array with repespect to cost matrix and source node
 	}
 	
-	visited[source_ver] = 1; // it initailize the index of visited array at source_ver as visisted
-	dist[source_ver] = 0;  // it initailize the index of distance array at source_ver as 0 because distance from source_ver to source_ver is 0
+	visited[source_node] = 1; // it initailize the index of visited array at source_node as visisted
+	dist[source_node] = 0;  // it initailize the index of distance array at source_node as 0 because distance from source_node to source_node is 0
 
-//this for loop finds the shortest path from source vertex to every other vertex
+//this for loop finds the shortest path from source node to every other node
 	
-	//this loop has to run for every vertex apart from the source vertex therefore it runs totver-1 times
-	for (int i = 1; i <= totver-1; i++)
+	//this loop has to run for every node apart from the source node therefore it runs tot_node-1 times
+	for (int i = 1; i <= tot_node-1; i++)
 	{
 		min = INFINITY; // considering minimum as infinity
 
-		for (int j = 1; j <= totver; j++)
+		for (int j = 1; j <= tot_node; j++)
 		{
 			// checking weather it is neighbouring vextex, weather distance is smaller than minimum and it is visited or not
 			if (dist[j] < min && visited[j] == 0) 
@@ -59,10 +59,10 @@ int main()
 				visited[j] = 1; // making current j index of visited array as visited
 			}
 
-			// again checking every vertex from this(j) neighbouring vextex
-			for (int k = 1; k <= totver; k++)
+			// again checking every node from this(j) neighbouring vextex
+			for (int k = 1; k <= tot_node; k++)
 			{
-				//checking weather the distance of dist[j](neighbouring vextex) + cost[j][k](neighbouring vextex to other vertex) is smaller than dist[k](other vertex)
+				//checking weather the distance of dist[j](neighbouring vextex) + cost[j][k](neighbouring vextex to other node) is smaller than dist[k](other node)
 				if (dist[j] + cost[j][k] < dist[k]) 
 				{
 					dist[k] = cost[j][k] + dist[j];
@@ -70,20 +70,18 @@ int main()
 			}
 		}
 	}
-	
 	// printing the shortest path discovered 
 	printf("\nShortest Paths: \n");
-	for (int i = 1; i <= totver; i++)
+	for (int i = 1; i <= tot_node; i++)
 	{
-		if (i != source_ver)
+		if (i != source_node)
 		{
-			printf("\t%d->%d = %d\n", source_ver, i, dist[i]);
+			printf("\t%d->%d = %d\n", source_node, i, dist[i]);
 		}
 	}
-	
-	// check's weather the user what to try with diffrent source vertex
+	// check's weather the user what to try with diffrent source node
 	switch_invalid:
-	printf("\nIf you want find shortest path from other source vertex than press :1\n");
+	printf("\nIf you want find shortest path from other source node than press :1\n");
 	printf("To exit press :0\n");
 	scanf("%d", &start_again); 
 	switch (start_again)
